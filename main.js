fc41f4c16bcb5be7f7305bf2c393615146c37df8
Code Programer <93539221+code-programer@users.noreply.github.com>
2023-01-02 19:34:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,45 @@
+//define db
 var db = {};
-//Write here your db url
-db.url;
-db.loadData = function(){
-  fetch(db.url)
-  .then(response => response.json())
-  .then(data => return data);
+ 
+//define db functions
+db.add = function(k, v, id="b36e04dc-c473-44bf-ad87-0e49fc5060b5", bas="database"){
+	var myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+
+	var raw = JSON.stringify({
+  		k: v
+	});
+
+	var requestOptions = {
+  		method: 'PUT',
+  		headers: myHeaders,
+  		body: raw,
+  		redirect: 'follow'
+	};
+
+	
+	fetch("https://getpantry.cloud/apiv1/pantry/" + id + "/basket/" + bas + "/", requestOptions)
+  		.then(response => response.text())
+  		.then(result => console.log(result))
+  		.catch(error => console.log('error', error));
 }
-db.editData(variable, content){
-  
+
+
+db.data; 
+
+
+db.get = function(id="b36e04dc-c473-44bf-ad87-0e49fc5060b5",bas="database"){
+	var myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+
+	var requestOptions = {
+		method: 'GET',
+		headers: myHeaders,
+		redirect: 'follow'
+	};
+	
+	fetch("https://getpantry.cloud/apiv1/pantry/" + id + "/basket/" + bas + "/", requestOptions)
+	  	.then(response => response.text())
+	  	.then(result => db.data = result)
+	  	.catch(error => console.log('error', error));
 }
