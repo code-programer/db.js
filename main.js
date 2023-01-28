c5a9721943057e5a3689e62842b65ef5d39f06ec
Code Programer <93539221+code-programer@users.noreply.github.com>
2023-01-28 17:06:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-var db = {}
-db.data;
-
-function uptade(a, b, binId="63c2b481dfc68e59d58264cc"){
-	let req = new XMLHttpRequest();
+class DB{
+	constructor(binId, masterKey){
+		this.binId = binId;
+		this.masterKey = masterKey;
+		this.data = {};
+	}
+	uptade(a, b){
+		let req = new XMLHttpRequest();
 	
 	req.onreadystatechange = () => {
   		if (req.readyState == XMLHttpRequest.DONE) {
@@ -10,24 +13,26 @@ function uptade(a, b, binId="63c2b481dfc68e59d58264cc"){
 		  }
 	};
 	
-	req.open("PUT", "https://api.jsonbin.io/v3/b/" + binId, true);
+	req.open("PUT", "https://api.jsonbin.io/v3/b/" + this.binId, true);
 	req.setRequestHeader("Content-Type", "application/json");
-	req.setRequestHeader("X-Master-Key", "$2b$10$GQBXgX0qtSt9SMrLTUYkle8r3ine3QESBb8jK69jddclQRRmMufvS");
-	db.data[a] = b;
-	req.send(JSON.stringify(db.data));
-}
-
-function get(binId="63c2b481dfc68e59d58264cc", callback="console.log"){
+	req.setRequestHeader("X-Master-Key", this.masterKey);
+	this.data[a] = b;
+	req.send(JSON.stringify(this.data));
+	}
+	
+	//get function
+	get(callback="console.log"){
 	let req = new XMLHttpRequest();
 
 	req.onreadystatechange = () => {
 	  if (req.readyState == XMLHttpRequest.DONE) {
 	    eval(callback + "(" + req.responseText + ")")
-	    db.data = JSON.parse(req.responseText);
+	    this.data = JSON.parse(req.responseText);
 	  }
 	};
-
-	req.open("GET", "https://api.jsonbin.io/v3/b/" + binId + "?meta=false", true);
-	req.setRequestHeader("X-Master-Key", "$2b$10$GQBXgX0qtSt9SMrLTUYkle8r3ine3QESBb8jK69jddclQRRmMufvS");
+		
+	req.open("GET", "https://api.jsonbin.io/v3/b/" + this.binId + "?meta=false", true);
+	req.setRequestHeader("X-Master-Key", this.masterKey);
 	req.send();
+}
 }
